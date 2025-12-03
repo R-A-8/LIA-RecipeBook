646da83ed8f03806670461a1f6235d18fffa3cfc
Renda <abioulir@gmail.com>
2025-12-03 14:25:53 -0500

    Remade the classes.

---
+++ Recipes.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Write a description of class Recipes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Recipes
+{
+    private String name;
+    
+    private HashSet ingredientsList;
+    
+    private ArrayList stepList;
+    
+    public Recipes()
+    {
+        ingredientsList = new HashSet<String>();
+        stepList = new ArrayList<String>();
+    }
+    
+    public void addSteps()
+    {
+        
+    }
+    
+    public void removeSteps()
+    {
+        
+    }
+    
+    public void addIngredients()
+    {
+        
+    }
+    
+    public void removeIngredient()
+    {
+        
+    }
+    
+}
---
+++ Unit.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration Unit - écrire ici la description de l'énumération
+ *
+ * @author (votre nom)
+ * @version (numéro de version ou date)
+ */
+public enum Unit
+{
+    LUNDI, MARDI, MERCREDI, JEUDI, VENDREDI, SAMEDI, DIMANCHE
+}
